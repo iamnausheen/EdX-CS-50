b7337422f6db35339b6d03c347ac2619e51be7d0
iamnausheen <152173229+iamnausheen@users.noreply.github.com>
2026-09-09 14:44:32 +0530

    Add files via upload

---
+++ 3_Memory/volume/volume.c
@@ -0,0 +1,62 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+    int* header = malloc(HEADER_SIZE);
+    if (header == NULL)
+    {
+        printf("Memory related error\n");
+        return 2;
+    }
+    if (fread(header, HEADER_SIZE, 1, input) != 0)
+    {
+        fwrite(header, HEADER_SIZE, 1, output);
+    }
+
+    free(header);
+
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t sample;
+
+    while (fread(&sample, sizeof(sample), 1, input) != 0)
+    {
+        sample = sample * factor;
+        fwrite(&sample, sizeof(sample), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
